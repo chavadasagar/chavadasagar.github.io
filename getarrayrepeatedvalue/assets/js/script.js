1c349439455ea624fae44b09f61c1b214ae13a83
sagar chavada <66827189+chavadasagar@users.noreply.github.com>
2022-10-07 19:00:34 +0530

    Add files via upload

--- getarrayrepeatedvalue/assets/js/script.js
+++ getarrayrepeatedvalue/assets/js/script.js
@@ -1,27 +1,59 @@
 import { GetArrRepeatedValue } from "./modules/GetArrRepeatedValue.js";
+import { GetSeqWiseCountVal } from "./modules/GetSeqWiseCountVal.js";
 
-document.querySelector("#check").addEventListener("click", function () {
-    let UserString = document.getElementById("UserString").value;
-    let outpot = document.querySelector(".output");
-    debugger
-    if (UserString !== "") {
-        let tempArr = UserString.split("");
-        // outpot.innerText = JSON.stringify(GetArrRepeatedValue(tempArr));
-        outpot.innerHTML += `
+
+if (document.getElementById("check") != null) {
+    document.getElementById("check").addEventListener("click", function () {
+        let UserString = document.getElementById("UserString").value;
+        let outpot = document.querySelector(".output");
+        debugger
+        if (UserString !== "") {
+            let tempArr = UserString.split("");
+            // outpot.innerText = JSON.stringify(GetArrRepeatedValue(tempArr));
+            outpot.innerHTML += `
+         <table bordre=1>
+         `;
+            let outputdata = "<table border='1'>";
+            outputdata += "<tr><th>Value</th><th>Count</th></tr>";
+            Object.entries(GetArrRepeatedValue(tempArr)).forEach(val => {
+                outputdata += `<tr>
+             <td>${val[0]}</td>
+             <td>${val[1]}</td>
+             </tr>`;
+            });
+            outputdata += `</table>`;
+            outpot.innerHTML = outputdata;
+        }
+        else {
+            alert("Please Enter Value");
+        }
+    });
+}
+
+if (document.getElementById("sq_check_btn") != null) {
+    document.getElementById("sq_check_btn").addEventListener("click", function () {
+        let UserString = document.getElementById("UserString").value;
+        let outpot = document.querySelector(".output");
+        if (UserString !== "") {
+            // outpot.innerText = JSON.stringify(GetArrRepeatedValue(tempArr));
+            outpot.innerHTML += `
         <table bordre=1>
         `;
-        let outputdata = "<table border='1'>";
-        outputdata += "<tr><th>Value</th><th>Count</th></tr>";
-        Object.entries(GetArrRepeatedValue(tempArr)).forEach(val=>{
-            outputdata += `<tr>
-            <td>${val[0]}</td>
-            <td>${val[1]}</td>
+            let outputdata = "<table border='1'>";
+            outputdata += "<tr><th>Value</th><th>Count</th></tr>";
+            GetSeqWiseCountVal(UserString).forEach(val => {
+                debugger
+                outputdata +=
+                    `<tr>
+                <td>${val[0]}</td>
+                <td>${val[1]}</td>
             </tr>`;
-        });
-        outputdata += `</table>`;
-        outpot.innerHTML = outputdata;
-    }
-    else {
-        alert("Please Enter Value");
-    }
-});
+            });
+            outputdata += `</table>`;
+            outpot.innerHTML = outputdata;
+        }
+        else {
+            alert("Please Enter Value");
+        }
+    });
+}
