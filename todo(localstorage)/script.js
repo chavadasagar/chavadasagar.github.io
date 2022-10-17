0cc25f4f72ba90658ecb566608403cbd5f8851d7
sagar chavada <66827189+chavadasagar@users.noreply.github.com>
2022-10-17 10:44:18 +0530

    add guid module

--- todo(localstorage)/script.js
+++ todo(localstorage)/script.js
@@ -1,8 +1,22 @@
-
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
 function addtodo() {
     var todo = document.querySelector("#todo").value;
     if (localStorage.alltodo == undefined) {
-        localStorage.alltodo = JSON.stringify([{ id: 1, name: todo, isComplate: false ,createdTime:new Date() }]);
+        localStorage.alltodo = JSON.stringify([{ id: generateUUID(), name: todo, isComplate: false ,createdTime:new Date() }]);
         document.querySelector("#todo").value = "";
         Display();
     }
@@ -11,7 +25,7 @@ function addtodo() {
     }
     else {
         var temparr = JSON.parse(localStorage.alltodo);
-        var id = temparr.length + 1;
+        var id = generateUUID();
         temparr.push({ id: id, name: todo, isComplate: false });
         localStorage.alltodo = JSON.stringify(temparr);
         document.querySelector("#todo").value = "";
